dda5d629d414186ef33a9908e153536b76aab038
VictorSilva654 <victordejesus654@gmail.com>
2023-06-06 19:12:21 -0300

    Criando o arquivo TS

---
+++ src/main.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiplicacao = (valor1, valor2) => valor1 * valor2;
+const dizSeuNome = (nome) => `Olá ${nome}`;
+console.log(multiplicacao(4, 2));
+console.log(dizSeuNome("Astolfo"));
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+const multiplicacao = (valor1: number, valor2: number):number => valor1 * valor2;
+
+const dizSeuNome = (nome: string):string => `Olá ${nome}`;
+
+console.log(multiplicacao(4, 2));
+console.log(dizSeuNome("Astolfo"));
